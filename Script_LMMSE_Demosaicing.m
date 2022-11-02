28eb4b99cbd85b8695f83a235444cd002ada847c
Pierre-Jean Lapray <plapray@gmail.com>
2022-11-02 09:56:32 +0100

    Update Script_LMMSE_Demosaicing.m

--- Script_LMMSE_Demosaicing.m
+++ Script_LMMSE_Demosaicing.m
@@ -1,42 +1,56 @@
 %% This script load, and demosaic with LMMSE the data from the selected image
+% Reference : "Spote A, Lapray PJ, Thomas JB, Farup I. Joint demosaicing of
+%              colour and polarisation from filter arrays. 
+%              In 29th Color and Imaging Conference Final Program and Proceedings 2021. 
+%              Society for Imaging Science and Technology."
 clc
 close all
 clear all
 
 %% Global parameter
 D_Matrix_name = 'D_Matrix.mat';
-Save=false; %true to save the demosaiced image
-Save_name = 'DemosImg.mat';
+Save = false; % true to save the demosaiced image
 
 %% Add path to Matlab for access
 addpath(genpath('Function/'))
 
-%% Define input folder
-path=pwd();
-folder_path = strcat(path, '/Data/');
-
-%% Define output folder
-folder_path1 = strcat(path, '/Result/');
-
 %% Load mosaiced image
-MosImg=im2double(imread([folder_path 'im.tif']));
-figure;imshow(MosImg);
+MosImg=im2double(imread(['Data/im.tif'])); % (The scene has a polarizer sheet in background)
+figure;imshow(MosImg);title('Mosaiced image');
+
+%% Sizes definition
+height = 4;                                 % height of the superpixel
+width = 4;                                 % width of the superpixel
+nh = 10;                               % number of neighbors per column (this code is valid only for 10x10 neighborhood)
+nw= 10;                                % number of neighbors per line
+P = 12;                                % number of color-pola channels
+[rows, cols, ~] = size(MosImg);
+r_superpix = rows/height;               % number of superpixel in a line
+c_superpix = cols/width;                % number of superpixel in a column
 
 %% Load D_Matrix
-D=load([folder_path D_Matrix_name]).D;
+D=load(['Data/D_matrix.mat']).D;
 
 %% Demosaicing
-DemosImg = LMMSE_demosaicing(D, MosImg(:,:,1));
+fun = @(x) (reshape(x.data(:),1,1,nh*nw));
+x1 = permute(blockproc(MosImg,[4,4],fun,BorderSize=[3 3],TrimBorder=false,UseParallel =true),[3 1 2]);
+y_estimate = D*reshape(x1,nh*nw,r_superpix*c_superpix);
+
+%% Reconstruction of demosaicked images
+img_estimate = zeros(rows, cols, P);
+for u = 1:P
+    img_estimate(:,:,u) = col2im(y_estimate(height*width*(u-1)+1:height*width*u,:), [height width], [r_superpix*height c_superpix*width],'distinct');
+end
+DemosImg = reshape(img_estimate,[r_superpix*height c_superpix*width 3 4]);
 
 %% Show result
 figure;
-subplot(2,2,1),imshow(DemosImg(:,:,:,1)),title('RGB image for polarization of 0°')
-subplot(2,2,2),imshow(DemosImg(:,:,:,2)),title('RGB image for polarization of 45°')
-subplot(2,2,3),imshow(DemosImg(:,:,:,3)),title('RGB image for polarization of 90°')
-subplot(2,2,4),imshow(DemosImg(:,:,:,4)),title('RGB image for polarization of 135°')
+subplot(2,2,1),imshow(DemosImg(:,:,:,1)),title('Demosaiced image for 0° polarization')
+subplot(2,2,2),imshow(DemosImg(:,:,:,2)),title('Demosaiced image for 45° polarization')
+subplot(2,2,3),imshow(DemosImg(:,:,:,3)),title('Demosaiced image for 90° polarization')
+subplot(2,2,4),imshow(DemosImg(:,:,:,4)),title('Demosaiced image for 135° polarization')
 
 %% Save result
 if Save == true
-    save([folder_path1 Save_name],'DemosImg','-v7.3');
+    save(['DemosImg.mat'],'DemosImg','-v7.3');
 end
-
