13c10df675c3d07f8eb9475642f68184af54b3ab
Pierre-Jean Lapray <plapray@gmail.com>
2022-11-07 11:32:24 +0100

    Update Load_Dataset.m

--- Function/Load_Dataset.m
+++ Function/Load_Dataset.m
@@ -1,7 +1,7 @@
 % Function that converts the input dataset in an output which is used in
 % main to mosaiced the images.
 %%
-function Dataset = Load_Dataset(Save, folder)
+function Dataset = Load_Dataset(Save, folder, nbr_of_img)
 
 % ---------------------------------------------------------------------
 % All images are load as double.
@@ -14,15 +14,14 @@ function Dataset = Load_Dataset(Save, folder)
 
 path = [folder];
 filename = dir(path);
-Dataset = cell((length(filename)-2), 5);
-filenames = natsortfiles(filename);
-for k = 2:length(filename)-1
-    img_0 = im2double(imread([path, '0_' num2str(k-1) '.png']));
-    img_45 = im2double(imread([path, '45_' num2str(k-1) '.png']));
-    img_90 = im2double(imread([path,'90_' num2str(k-1) '.png']));
-    img_135 = im2double(imread([path, '135_' num2str(k-1) '.png']));
+Dataset = cell((nbr_of_img), 5);
+for k = 1:nbr_of_img
+    img_0 = im2double(imread([path, '0_' num2str(k) '.png']));
+    img_45 = im2double(imread([path, '45_' num2str(k) '.png']));
+    img_90 = im2double(imread([path,'90_' num2str(k) '.png']));
+    img_135 = im2double(imread([path, '135_' num2str(k) '.png']));
 
-    Dataset(k-1, 1:5) = {filenames(k).name, img_0, img_45, img_90, img_135};
+    Dataset(k, 1:5) = {num2str(k), img_0, img_45, img_90, img_135};
 end
 if Save == true
     save(['Data/FullDataset.mat'], 'Dataset', '-v7.3');
